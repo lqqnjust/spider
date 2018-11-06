f680a3176aab666960bcdfb364399306e8c91e88
lqqnjust <luqq1210@163.com>
2018-11-06 19:26:27 +0800

    清空

--- webui/scrapydm/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScrapydmConfig(AppConfig):
-    name = 'scrapydm'
--- webui/spidermanage/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpidermanageConfig(AppConfig):
-    name = 'spidermanage'
